86fbdfe71c9bb844b3e621779475ba1b2125bb2c
mandarup <mandarupadhye@gmail.com>
2017-04-22 13:19:49 -0400

    clean up

--- starter-package/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This is a starter package
-
-purpose of this package is to give a headstart to getting started
-with package development by getting rid of initial setup
-
-This package comes with pre-set sphinx documentation in ./docs as well
-as a virtual environment.
-"""
